8023c351375148745f1b76e06b82369fa2f2f0a3
pjkulkar <100871193+pjkulkar@users.noreply.github.com>
2022-03-06 16:43:03 -0800

    Create LikeX.js

---
+++ src/LikeX.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class LikeX extends React.Component {
+  state = {
+    itemsList: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const itemsList = res.data;
+        this.setState({ itemsList });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        {
+          this.state.itemsList
+            .map(item =>
+              <li key={item.id}></li>
+            )
+        }
+      </ul>
+    )
+  }
+}

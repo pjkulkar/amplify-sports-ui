647ec0435cedb7f6ef694d5d94c2d9b7fe472fcc
pjkulkar <100871193+pjkulkar@users.noreply.github.com>
2022-03-07 11:33:49 -0800

    Update LikeX.js

--- src/components/LikeX.js
+++ src/components/LikeX.js
@@ -24,7 +24,8 @@ export default class LikeX extends React.Component {
         console.log('movList item 0' + itemsList[0].itemId)
         this.setState({ itemsList})
         console.log('this.state.itemslist' )
-         console.log( this.state.itemsList);
+        console.log( this.state.itemsList)
+        console.log('this.state.itemsList item 0' + this.state.itemsList[0].itemId)
         if(this.state.itemsList){
           this.setState({isLoading:false, error:false});
           console.log("inside mount after setstate")
@@ -39,14 +40,19 @@ export default class LikeX extends React.Component {
 
   render() {
     console.log('in render: isloading ' + this.state.isLoading)
-    console.log('in render: itemsList ' + this.state.itemsList)
+    console.log('this.state.itemslist' )
+    console.log( this.state.itemsList)
+    console.log('this.state.itemsList item 0' + this.state.itemsList[0].itemId)
     console.log('in render: error ' + this.state.error)
     if (this.state.isLoading) return <div>Loading...</div>;
     if (this.state.error) return <div>There was an error:{this.state.error}</div>;
     
     console.log("inside render")
-    console.log(this.state.itemslist);
-        console.log("inside render before return")
+    console.log('this.state.itemslist' )
+    console.log( this.state.itemsList)
+    console.log('this.state.itemsList item 0' + this.state.itemsList[0].itemId)
+    
+    console.log("inside render before return")
     return (
       <ul>
         {

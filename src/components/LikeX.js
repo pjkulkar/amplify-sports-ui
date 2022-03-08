eef702fcc6eca49b9ea81a4196b3adb3d9d66245
pjkulkar <100871193+pjkulkar@users.noreply.github.com>
2022-03-07 22:18:47 -0800

    Update LikeX.js

--- src/components/LikeX.js
+++ src/components/LikeX.js
@@ -59,7 +59,7 @@ export default class LikeX extends React.Component {
           <table>
             <thead>
               <tr>
-                <th colspan=2>Movies Like X</th>
+                <th>Movies Like X</th>
                </tr>
               <tr>
                 <th>Movie Id</th>

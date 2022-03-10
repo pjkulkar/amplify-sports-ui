6dce62d029924663cefd3ad76e103c67690932b6
pjkulkar <100871193+pjkulkar@users.noreply.github.com>
2022-03-09 22:26:31 -0800

    Update TopPicks.js

--- src/components/TopPicks.js
+++ src/components/TopPicks.js
@@ -82,21 +82,7 @@ export default class TopPicks extends React.Component {
           </table>
         
       </div>
-      
-  <div class="row">
-    <div class="column" style="background-color:#aaa;">
-      <h2>Column 1</h2>
-      <p>Some text..</p>
-    </div>
-    <div class="column" style="background-color:#bbb;">
-      <h2>Column 2</h2>
-      <p>Some text..</p>
-    </div>
-    <div class="column" style="background-color:#ccc;">
-      <h2>Column 3</h2>
-      <p>Some text..</p>
-    </div>
-</div>
+
     
       </div>
       

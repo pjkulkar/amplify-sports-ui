2018ebda800bc08ce67cb2a0b31b8e96b595c666
pjkulkar <100871193+pjkulkar@users.noreply.github.com>
2022-03-09 19:31:24 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,10 +18,100 @@ import { MdSend /* MdList */ } from 'react-icons/md'
 import axios from 'axios' 
 import awsConfig from './aws-exports'
 
+const thumbs_up = require('./assets/thumbs_up.png');
+const thumbs_down = require('./assets/thumbs_down.png');
 
 Amplify.configure(awsConfig) 
 
+  
+class VideoPlayer extends React.Component {
 
+  componentDidMount() {
+    this.player = videojs(this.videoNode, this.props);
+  }
+
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose()
+    }
+  }
+
+  render() {
+    return (
+      
+      <div data-vjs-player style={{
+          width: 540, height: 320
+        }}>
+        <video  ref={(node) => { this.videoNode = node; }} className="video-js" />
+      </div>
+      
+
+    );
+  }
+}
+
+
+const useFetchData = (url) => {
+  const [state, setState] = useState({ isLoading: true, error: null, data: null });
+  useEffect(() => {
+    //let isMounted = true;  
+    axios.get(url)
+      .then((res) => {
+        console.log(res.data.Items.length)
+        if(res.data.Items.length === 3){
+          setState(
+          { isLoading: false, data: [
+            {autoplay: false, controls: true,sources: [{src: res.data.Items[0].filepath.S}]},
+            {autoplay: false, controls: true,sources: [{src: res.data.Items[1].filepath.S}]},
+            {autoplay: false, controls: true,sources: [{src: res.data.Items[2].filepath.S}]}], 
+            error: null });
+        } else if (res.data.Items.length === 4){
+          setState(
+          { isLoading: false, data: [
+            {autoplay: false, controls: true,sources: [{src: res.data.Items[0].filepath.S}]},
+            {autoplay: false, controls: true,sources: [{src: res.data.Items[1].filepath.S}]},
+            {autoplay: false, controls: true,sources: [{src: res.data.Items[2].filepath.S}]},
+            {autoplay: false, controls: true,sources: [{src: res.data.Items[3].filepath.S}]}], 
+            error: null });
+        } else if (res.data.Items.length === 5){
+          setState(
+          { isLoading: false, data: [
+            {autoplay: false, controls: true,sources: [{src: res.data.Items[0].filepath.S}]},
+            {autoplay: false, controls: true,sources: [{src: res.data.Items[1].filepath.S}]},
+            {autoplay: false, controls: true,sources: [{src: res.data.Items[2].filepath.S}]},
+            {autoplay: false, controls: true,sources: [{src: res.data.Items[3].filepath.S}]},
+            {autoplay: false, controls: true,sources: [{src: res.data.Items[4].filepath.S}]}], 
+            error: null });
+        }
+      })
+      .catch((error) => {
+        setState({ isLoading: false, data: null, error });
+      });
+  }, [url]);
+  return state;
+};
+
+function populateDate(username,video,vote){
+    console.log(username,video,vote);
+    axios.post('https://dcyxom2xcc.execute-api.us-east-1.amazonaws.com/prod/updaterecord', {
+      username: username,
+      video: video,
+      vote: vote
+    })
+  };
+
+function getLikeXList (itemId,userId) { 
+console.log("inside getLikeX")
+const query = "https://bt1m7jsjsj.execute-api.us-west-2.amazonaws.com/test/likex?itemId=" + itemId + "&userId=" + userId;
+console.log(query);
+axios.get(query)
+.then(res => {
+console.log(res.data.itemList)
+console.log("called lambdA") 
+}).catch(err =>{
+console.log(err);
+})
+}
 
 function useForceUpdate() {
   let [value, setState] = useState(true);
@@ -37,6 +127,9 @@ const [userId, setuserId] = useState(10);
 
 const [movieId, setmovieId] = useState(22);
 
+  const { isLoading, data, error } = useFetchData("https://56lor2kfz8.execute-api.us-east-1.amazonaws.com/test/videos");
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>There was an error: {error}</div>;
    
 
     return(
@@ -91,6 +184,20 @@ const [movieId, setmovieId] = useState(22);
        <div>                                                                       
           <LikeWatched/>
       </div>
+<div>       
+<table>
+          <tbody>
+            <tr>
+              <th>Video</th>
+              <th>Vote</th>
+            </tr>
+            {data.map(function(object, i){
+              //console.log(object);
+                return <tr><td><VideoPlayer { ...object  }/></td><td><img src={thumbs_up} alt="Thumbs Up" onClick={() => populateDate('hnvasa@gmail.com',object.sources[0].src,'upvote')} /><img src={thumbs_down} alt="Thumbs Down" onClick={() => populateDate('hnvasa@gmail.com',object.sources[0].src,'downvote')} /></td></tr>;
+            })}
+          </tbody>
+        </table>
+      </div>
      
                                                                                                                                                         
                                                                                 
@@ -110,4 +217,3 @@ const container = { paddingTop: 40, width: 960, margin: '0 auto' }
 const navHeading = { margin: 0, fontSize: 18 }
 
 export default App
-

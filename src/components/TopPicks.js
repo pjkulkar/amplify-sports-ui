11d954cced79ddbb1902961faafd20007bbaa1c3
pjkulkar <100871193+pjkulkar@users.noreply.github.com>
2022-03-10 09:22:43 -0800

    Update TopPicks.js

--- src/components/TopPicks.js
+++ src/components/TopPicks.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import axios from 'axios';
-  
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import { withStyles } from "@material-ui/core/styles";
+
 export default class TopPicks extends React.Component {
   
   constructor(props){
